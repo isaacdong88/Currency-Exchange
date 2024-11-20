da0d0809cd3a4dfbf353132759b5b5343442a585
isaacdong88 <isaacdong88@gmail.com>
2024-11-20 16:36:06 -0500

    testing user input for conversion values and getting resquest from API

--- currencyAPI/currency.py
+++ currencyAPI/currency.py
@@ -1,8 +1,13 @@
 import requests
 
+from_currency = input("Convert Currency From: ")
+to_currency = input("Convert Currency To: ")
+amount = input("Amount: ")
+
+
 url = "https://currency-converter-pro1.p.rapidapi.com/convert"
 
-querystring = {"from": "USD", "to": "EUR", "amount": "100"}
+querystring = {"from": from_currency, "to": to_currency, "amount": amount}
 
 headers = {
     "x-rapidapi-key": "1b87ac329bmshf729da2c229352ep18128ejsn179d7c8d95db",
