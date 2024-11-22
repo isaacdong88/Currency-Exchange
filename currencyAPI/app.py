3e7290d7b2eeeefd9a70dd420bf50f4b5fb892e8
isaacdong88 <isaacdong88@gmail.com>
2024-11-21 20:45:13 -0500

    create desktop GUI using tkinter

---
+++ currencyAPI/app.py
@@ -0,0 +1,10 @@
+from tkinter import Tk, ttk
+from tkinter import *
+
+window = Tk()
+window.geometry('400x400')
+window.title('Currency Converter')
+window.configure(bg='red')
+window.resizable(height=FALSE, width=FALSE)
+
+window.mainloop()
